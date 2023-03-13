29994a6d71302c386f1416183543e961c3c4c1f1
Youssef Soliman <yosfsoliman151@gmail.com>
2023-03-13 21:46:53 +0200

    add code of spinner

--- app/src/main/java/com/example/calculatortask/MainActivity.kt
+++ app/src/main/java/com/example/calculatortask/MainActivity.kt
@@ -3,10 +3,17 @@ package com.example.calculatortask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.Spinner
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private val Binary = "Binary"
+    private val Hexa = "Hexa"
+    private val Octa= "Octa"
+    private val Decimal = "Decimal"
 
     lateinit var buttonOne:Button
     lateinit var buttonTwo:Button
@@ -25,8 +32,11 @@ class MainActivity : AppCompatActivity() {
     lateinit var buttonE:Button
     lateinit var buttonF:Button
     lateinit var clearButton:Button
+    lateinit var equalButton:Button
     lateinit var buttonFloatPoint:Button
     lateinit var textNumber:TextView
+    lateinit var spinnerFrom:Spinner
+    lateinit var spinnerTo:Spinner
         override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -38,7 +48,43 @@ class MainActivity : AppCompatActivity() {
         clearButton.setOnClickListener{
         clear()
         }
+        equalButton.setOnClickListener{
+            convert(textNumber.text.toString(),getFrom(),getTo())
+        }
+
     }
+
+    private fun convert(text:String,from:Int,to:Int){
+
+        val decmal=text.toInt(from)
+        val result= decmal.toString(to)
+    }
+
+
+    private fun getFrom():Int {
+        var input =
+            return when (input) {
+                "Binary" -> 2
+                "Octal" -> 8
+                "Decimal" -> 10
+                "Hexa" -> 16
+
+            }
+    }
+
+    private fun getTo():Int {
+        var input =
+            return when (input) {
+                "Binary" -> 2
+                "Octal" -> 8
+                "Decimal" -> 10
+                "Hexa" -> 16
+
+            }
+    }
+
+
+
 
     fun clear(){
         textNumber.text=""
@@ -46,11 +92,20 @@ class MainActivity : AppCompatActivity() {
     private fun initView(){
         clearButton=findViewById(R.id.button_digit_delete)
         textNumber =findViewById(R.id.text_number)
+        spinnerFrom=findViewById(R.id.spinner1)
+        spinnerTo=findViewById(R.id.spinner2)
     }
     fun onClickNumber(v: View){
         val newDigit = (v as Button).text.toString()
         val oldDigits = textNumber.text.toString()
         val newTextNumber = oldDigits + newDigit
         textNumber.text = newTextNumber
+    }
+
+    private fun populateSpinnerMenu() {
+        val listOfBases = listOf(Binary,Hexa,Octa,Decimal)
+        val adapter = ArrayAdapter(this, R.layout.spinner, listOfBases)
+        spinnerFrom.setAdapter(adapter)
+        spinnerTo.setAdapter(adapter)
     }
 }
